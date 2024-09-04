964b5edd150ef741127aa07e85069f9e2c559b29
Julian Mojica <julian.mojica@amexgbt.com>
2024-09-04 11:04:20 -0300

    Grid structure basic

--- weather-dashboard/src/App.jsx
+++ weather-dashboard/src/App.jsx
@@ -2,33 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid2';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        
+            <Grid key={1} size={{ xs: 2, sm: 4, md: 6 }}>
+              Hello
+            </Grid>
+            <Grid key={1} size={{ xs: 2, sm: 4, md: 6 }}>
+              Hello
+            </Grid>
+            <Grid key={1} size={{ xs: 2, sm: 4, md: 6 }}>
+              Hello
+            </Grid>
+            <Grid key={1} size={{ xs: 2, sm: 4, md: 6, lg: 6 }}>
+              Hello
+            </Grid>
+
+          
+        </Grid>
+    </Box>
+    </div>
   )
 }
 
