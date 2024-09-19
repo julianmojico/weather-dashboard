93dbf1af4357dc7fbc5449787f8252ece8d07a6f
Gustavo Tajes Genga <gustavo.tajes@amexgbt.com>
2024-09-19 09:15:02 -0300

    Layout proposal. Replace cards with paper component

--- src/components/Week/Week.jsx
+++ src/components/Week/Week.jsx
@@ -1,87 +1,105 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid2';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import CardActionArea from '@mui/material/CardActionArea';
-import WbCloudyIcon from '@mui/icons-material/WbCloudy';
-import AcUnitIcon from '@mui/icons-material/AcUnit';
-import ThunderstormIcon from '@mui/icons-material/Thunderstorm';//Icono Tormenta
 import WbSunnyIcon from '@mui/icons-material/WbSunny'; //Solcito
-import { SvgIcon } from '@mui/material';
+import Paper from '@mui/material/Paper';
 
-export default function Week(){
+export default function Week() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        <Box sx={{flexGrow: 1}}>
+            <Grid container spacing={{xs: 2, md: 3}}>
 
-          <Grid key={1} size={{ xs: 2, sm: 4, md: 6 }}>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-              <WbSunnyIcon />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Bariloche
-                  </Typography>
-                  <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                    Sunny windy weather today but we love eating chocolates and hiking in the mountains with a lot of snow and skiing super fast.
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-          
-          <Grid key={1} size={{ xs: 2, sm: 4, md: 6 }}>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-              <WbSunnyIcon />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Moron
-                  </Typography>
-                  <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                    Sunny windy weather today. 
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-          
-          <Grid key={1} size={{ xs: 2, sm: 4, md: 6 }}>
-          <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-              <ThunderstormIcon fontSize='large' htmlColor='blue'/>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Tandil
-                  </Typography>
-                  <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                    The forecast for Tandil city shows very have storms with possibility of tornado, earthquake
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-          <Grid key={1} size={{ xs: 2, sm: 4, md: 6, lg: 6 }}>
-          <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-              <AcUnitIcon />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    Mar del Plata
-                  </Typography>
-                  <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                    Mardel people are a widespread group of squamate reptiles, with over 6,000
-                    species, ranging across all streets except Champagnat
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
+                <Grid item key={1} size={{xs: 12, sm: 12, md: 3}}>
+                    <Paper elevation={6} sx={{maxWidth: 1, borderRadius: 2}}>
+                        <WbSunnyIcon/>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Bariloche
+                        </Typography>
+                        <Typography variant="body2" sx={{color: 'text.secondary'}}>
+                            Sunny windy weather today but we love eating chocolates and hiking in the mountains
+                            with a lot of snow and skiing super fast.
+                        </Typography>
+                    </Paper>
+                </Grid>
 
+                <Grid item key={1} size={{xs: 12, sm: 12, md: 1.5}}>
+                    <Paper elevation={6} sx={{maxWidth: 1, borderRadius: 2}}>
+                        <WbSunnyIcon/>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Bariloche
+                        </Typography>
+                        <Typography variant="body2" sx={{color: 'text.secondary'}}>
+                            Sunny windy weather today but we love eating chocolates and hiking in the mountains
+                            with a lot of snow and skiing super fast.
+                        </Typography>
+                    </Paper>
+                </Grid>
 
-        </Grid>
-      </Box>
+                <Grid item key={1} size={{xs: 12, sm: 12, md: 1.5}}>
+                    <Paper elevation={6} sx={{maxWidth: 1, borderRadius: 2}}>
+                        <WbSunnyIcon/>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Bariloche
+                        </Typography>
+                        <Typography variant="body2" sx={{color: 'text.secondary'}}>
+                            Sunny windy weather today but we love eating chocolates and hiking in the mountains
+                            with a lot of snow and skiing super fast.
+                        </Typography>
+                    </Paper>
+                </Grid>
+                <Grid item key={1} size={{xs: 12, sm: 12, md: 1.5}}>
+                    <Paper elevation={6} sx={{maxWidth: 1, borderRadius: 2}}>
+                        <WbSunnyIcon/>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Bariloche
+                        </Typography>
+                        <Typography variant="body2" sx={{color: 'text.secondary'}}>
+                            Sunny windy weather today but we love eating chocolates and hiking in the mountains
+                            with a lot of snow and skiing super fast.
+                        </Typography>
+                    </Paper>
+                </Grid>
+
+                <Grid item key={1} size={{xs: 12, sm: 12, md: 1.5}}>
+                    <Paper elevation={6} sx={{maxWidth: 1, borderRadius: 2}}>
+                        <WbSunnyIcon/>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Bariloche
+                        </Typography>
+                        <Typography variant="body2" sx={{color: 'text.secondary'}}>
+                            Sunny windy weather today but we love eating chocolates and hiking in the mountains
+                            with a lot of snow and skiing super fast.
+                        </Typography>
+                    </Paper>
+                </Grid>
+
+                <Grid item key={1} size={{xs: 12, sm: 12, md: 1.5}}>
+                    <Paper elevation={6} sx={{maxWidth: 1, borderRadius: 2}}>
+                        <WbSunnyIcon/>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Bariloche
+                        </Typography>
+                        <Typography variant="body2" sx={{color: 'text.secondary'}}>
+                            Sunny windy weather today but we love eating chocolates and hiking in the mountains
+                            with a lot of snow and skiing super fast.
+                        </Typography>
+                    </Paper>
+                </Grid>
+
+                <Grid item key={1} size={{xs: 12, sm: 12, md: 1.5}}>
+                    <Paper elevation={6} sx={{maxWidth: 1, borderRadius: 2}}>
+                        <WbSunnyIcon/>
+                        <Typography gutterBottom variant="h5" component="div">
+                            Bariloche
+                        </Typography>
+                        <Typography variant="body2" sx={{color: 'text.secondary'}}>
+                            Sunny windy weather today but we love eating chocolates and hiking in the mountains
+                            with a lot of snow and skiing super fast.
+                        </Typography>
+                    </Paper>
+                </Grid>
+
+            </Grid>
+        </Box>
     )
 }
