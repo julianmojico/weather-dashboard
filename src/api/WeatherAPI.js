360f4dd97ac5b48dd3a2a1423cbe8eb33d227173
Gustavo Tajes Genga <gustavo.tajes@amexgbt.com>
2024-10-17 09:05:39 -0300

    Mock API response

--- src/api/WeatherAPI.js
+++ src/api/WeatherAPI.js
@@ -1,8 +1,13 @@
-export default function apiCall() {
-   return fetch('http://dataservice.accuweather.com/forecasts/v1/daily/5day/5878?' + new URLSearchParams({
+export default async function apiCall() {
+    const response = await fetch('http://localhost:3001/weather')
+    return await response.json()
+
+/*
+    return fetch('http://dataservice.accuweather.com/forecasts/v1/daily/5day/5878?' + new URLSearchParams({
       apikey: 'wKKjwferXpVkzLZ44Yuv4vu1lagbR8Nr',
       detail: true
     }, {
         mode: 'no-cors'
     }));
+*/
 }
